d061053f724a00d6ca603d3abe92e494154d84c0
Aryo Pratama <aryonp@gmail.com>
2015-08-21 20:18:31 +0700

    upd 1
    template SQL utk 2 RDS. Open for improvement

---
+++ hai-area-pgsql.sql
@@ -0,0 +1,86 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.4.4
+-- Dumped by pg_dump version 9.4.4
+-- Started on 2015-08-21 20:15:06 WIB
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- TOC entry 173 (class 3079 OID 12123)
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- TOC entry 2263 (class 0 OID 0)
+-- Dependencies: 173
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- TOC entry 172 (class 1259 OID 17871)
+-- Name: hai-area; Type: TABLE; Schema: public; Owner: postgres; Tablespace: 
+--
+
+CREATE TABLE "hai-area" (
+    id integer,
+    name text,
+    attrib text,
+    coord json,
+    "createID" integer,
+    "createDate" timestamp with time zone,
+    "updID" integer,
+    "updDate" timestamp with time zone,
+    del boolean
+);
+
+
+ALTER TABLE "hai-area" OWNER TO postgres;
+
+--
+-- TOC entry 2255 (class 0 OID 17871)
+-- Dependencies: 172
+-- Data for Name: hai-area; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY "hai-area" (id, name, attrib, coord, "createID", "createDate", "updID", "updDate", del) FROM stdin;
+\.
+
+
+--
+-- TOC entry 2262 (class 0 OID 0)
+-- Dependencies: 5
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+-- Completed on 2015-08-21 20:15:06 WIB
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ hai-area.mysql.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.12
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Aug 21, 2015 at 08:14 PM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hai`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hai-area`
+--
+
+DROP TABLE IF EXISTS `hai-area`;
+CREATE TABLE IF NOT EXISTS `hai-area` (
+  `id` int(11) NOT NULL,
+  `name` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `attrib` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `coord` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `createID` int(11) NOT NULL,
+  `createDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updID` int(11) NOT NULL,
+  `updDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `del` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `hai-area`
+--
+ALTER TABLE `hai-area`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `hai-area`
+--
+ALTER TABLE `hai-area`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
